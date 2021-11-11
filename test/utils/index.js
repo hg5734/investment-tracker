678b663c8ad30c355f5e18888231fdf815df6a50
hg5734 <himanshugoyal5734@gmail.com>
2021-11-11 10:35:01 +0400

    change in export and display balance

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,10 +1,14 @@
 
-export const displayBalance = (balances) =>{
-    let protocol = balances[i];
+const displayBalance = (balances) => {
+    let protocol = balances[0];
     let tokens = protocol[1];
     for (let j = 0; j < tokens.length; j++) {
         console.log({
-            [tokens[j][0]] : tokens[j][1].toString()
+            [tokens[j][0]]: tokens[j][1].toString()
         });
     }
+}
+
+module.exports = {
+    displayBalance
 }
